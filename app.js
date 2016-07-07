cdc0862091a4babb5cc712cefc9b7e72b7a7075f
yevelnad <yevelnad@gmail.com>
2016-07-07 14:34:50 -0800

    adding

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require('express');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static(__dirname + '/public'));
-app.listen(3000, function() { console.log('listening')});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
