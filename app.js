5cc6249385e6e7198087921260b5ab445d704717
yevelnad <yevelnad@gmail.com>
2016-07-07 14:43:00 -0800

    adding error

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
